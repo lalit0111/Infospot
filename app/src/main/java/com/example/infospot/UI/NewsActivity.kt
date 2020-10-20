3aebbbea2a71077541fd5cda419bd309d3250ff7
lalit0111 <tilwanil818@gmail.com>
2020-10-20 19:53:57 +0530

    Setup Breaking News Fragment Layout.

--- app/src/main/java/com/example/infospot/UI/NewsActivity.kt
+++ app/src/main/java/com/example/infospot/UI/NewsActivity.kt
@@ -2,14 +2,11 @@ package com.example.infospot.UI
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.viewpager2.widget.ViewPager2
 import com.example.infospot.Adapters.FragmentViewPagerAdapter
-import com.example.infospot.fragments.BreakingNewsFragment
 import com.example.infospot.R
+import com.example.infospot.fragments.BreakingNewsFragment
 import com.example.infospot.fragments.SavedNewsFragment
 import com.example.infospot.fragments.SearchNewsFragment
-import com.fxn.OnBubbleClickListener
 import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
@@ -17,18 +14,24 @@ class NewsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
 
+        //Creating Fragment Instances
         val breakingNewsFragment = BreakingNewsFragment()
         val savedNewsFragment = SavedNewsFragment()
         val searchNewsFragment = SearchNewsFragment()
 
+        //Creating Veiwpager Adapter
         val viewPagerAdapter = FragmentViewPagerAdapter(supportFragmentManager)
 
+        //Adding Fragments To Viewpager
         viewPagerAdapter.addFragment(savedNewsFragment)
         viewPagerAdapter.addFragment(breakingNewsFragment)
         viewPagerAdapter.addFragment(searchNewsFragment)
-        fragmentViewPager.adapter=viewPagerAdapter
 
-        bottomNavigationView.setupBubbleTabBar(viewPager = fragmentViewPager)
+        //Setting Adapter To Viewpager
+        fragmentViewPager.adapter = viewPagerAdapter
+
+        //Setting Viewpager To TabBar
+        bottomTabBar.setupBubbleTabBar(viewPager = fragmentViewPager)
 
     }
 }
